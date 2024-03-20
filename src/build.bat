0af8c5d33fb3d638bd07e18360bf7b5fd47d9582
dj-viking <anders.swedishviking@gmail.com>
2024-03-20 10:56:41 -0400

    use multithreaded compilation mode

--- src/build.bat
+++ src/build.bat
@@ -7,7 +7,7 @@ set windows_kits="C:\Program Files (x86)\Windows Kits"
 
 set visual_studio="C:\Program Files\Microsoft Visual Studio"
 
-cl /nologo /ZI /c /I. /IC:\qt5-build\qtbase\include /IC:\qt5-build\qtbase\include\QtWidgets\ /IC:\Users\ander\projects\hello-qt\Qt\5.15.2\msvc2019_64\include\ /I%windows_kits%\10\Include\10.0.22621.0\shared /I%windows_kits%\10\Include\10.0.22621.0\um /I%visual_studio%\2022\Community\VC\Tools\MSVC\14.37.32822\include\ /I%windows_kits%\10\Include\10.0.22621.0\ucrt ..\src\hello-qt.cpp ..\src\GetLastErrorAsString.cpp
+cl /MP /nologo /ZI /c /IC:\qt5-build\qtbase\include /IC:\qt5-build\qtbase\include\QtWidgets\ /IC:\Users\ander\projects\hello-qt\Qt\5.15.2\msvc2019_64\include\ /I%windows_kits%\10\Include\10.0.22621.0\shared /I%windows_kits%\10\Include\10.0.22621.0\um /I%visual_studio%\2022\Community\VC\Tools\MSVC\14.37.32822\include\ /I%windows_kits%\10\Include\10.0.22621.0\ucrt ..\src\hello-qt.cpp ..\src\GetLastErrorAsString.cpp
 echo "error level %errorlevel%"
 
 if NOT %errorlevel% == 0 (
