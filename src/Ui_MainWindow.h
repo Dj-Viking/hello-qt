5cc41d23c0dad21e51941067a077ea4913fa62e1
dj-viking <anders.swedishviking@gmail.com>
2024-03-24 00:09:18 -0400

    bigger button

--- src/Ui_MainWindow.h
+++ src/Ui_MainWindow.h
@@ -1,17 +1,19 @@
 /********************************************************************************
-** Form generated from reading UI file 'hello-qtorBvMS.ui'
+** Form generated from reading UI file 'hello-qtyQZdmf.ui'
 **
 ** Created by: Qt User Interface Compiler version 5.12.12
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
 
-#ifndef HELLO_2D_QTORBVMS_H
-#define HELLO_2D_QTORBVMS_H
+#ifndef HELLO_2D_QTYQZDMF_H
+#define HELLO_2D_QTYQZDMF_H
 
 #include <QtCore/QVariant>
+#include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
 #include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenu>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
@@ -25,6 +27,7 @@ public:
     QWidget *centralwidget;
     QPushButton *pushButton;
     QMenuBar *menubar;
+    QMenu *menuhello_world;
     QStatusBar *statusbar;
 
     void setupUi(QMainWindow *MainWindow)
@@ -36,15 +39,19 @@ public:
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         pushButton = new QPushButton(centralwidget);
         pushButton->setObjectName(QString::fromUtf8("pushButton"));
-        pushButton->setGeometry(QRect(340, 150, 93, 28));
+        pushButton->setGeometry(QRect(330, 190, 451, 331));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
         menubar->setGeometry(QRect(0, 0, 1116, 26));
+        menuhello_world = new QMenu(menubar);
+        menuhello_world->setObjectName(QString::fromUtf8("menuhello_world"));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
         MainWindow->setStatusBar(statusbar);
+
+        menubar->addAction(menuhello_world->menuAction());
 
         retranslateUi(MainWindow);
 
@@ -54,7 +61,8 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
-        pushButton->setText(QApplication::translate("MainWindow", "click me", nullptr));
+        pushButton->setText(QApplication::translate("MainWindow", "hello world", nullptr));
+        menuhello_world->setTitle(QApplication::translate("MainWindow", "hello world", nullptr));
     } // retranslateUi
 };
 
@@ -67,4 +75,4 @@ namespace Ui
 
 QT_END_NAMESPACE
 
-#endif // HELLO_2D_QTORBVMS_H
+#endif // HELLO_2D_QTYQZDMF_H
